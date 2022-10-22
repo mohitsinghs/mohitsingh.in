09b556e1a59d7ee0851f04c5bd8ffb9bf86f687d
Mohit Singh <mohit@mohitsingh.in>
2022-10-22 11:21:10 +0530

    fix: stats alignment in simple card

--- components/card/simple-card.js
+++ components/card/simple-card.js
@@ -23,16 +23,16 @@ export function SimpleCard({
       >
         <h3 className='mb-2 text-xl font-bold text-slate-600'>{title}</h3>
         {date && (
-          <p className='mb-2 text-sm font-medium text-slate-600 md:text-xs inline-flex items-center'>
+          <p className='mb-2 text-xs font-medium text-slate-600 inline-flex items-center flex-wrap gap-2'>
             <span className='mr-2.5'>
               <Calendar size={12} className='inline mr-1.5' />
               {date}
             </span>
-            <span className='ml-2.5'>
+            <span className='mr-2.5'>
               <Clock size={12} className='inline mr-1.5' />
               {time}
             </span>
-            <span className='ml-2.5'>
+            <span className='mr-2.5'>
               <TextLeft size={12} className='inline mr-1.5' />
               {words}
             </span>
