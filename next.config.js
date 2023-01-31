62bf672a60b42792fa2f813282965cbb38bb725e
Mohit Singh <mohit@mohitsingh.in>
2023-01-31 08:57:17 +0530

    fix: next image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withMarkdoc = require('@markdoc/next.js')
 module.exports = withMarkdoc()({
   reactStrictMode: true,
   pageExtensions: ['js', 'md'],
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
